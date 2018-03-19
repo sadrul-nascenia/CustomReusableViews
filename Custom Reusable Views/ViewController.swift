2e2281106c9aef07cdbc8275058090e11bee96e2
sadrul-nascenia <sadrul@bdipo.com>
2018-03-19 15:15:06 +0600

    Initial Commit

---
+++ Custom Reusable Views/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Custom Reusable Views
+//
+//  Created by ITADMIN on 3/19/18.
+//  Copyright © 2018 ITADMIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Custom Reusable ViewsTests/Custom_Reusable_ViewsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Custom_Reusable_ViewsTests.swift
+//  Custom Reusable ViewsTests
+//
+//  Created by ITADMIN on 3/19/18.
+//  Copyright © 2018 ITADMIN. All rights reserved.
+//
+
+import XCTest
+@testable import Custom_Reusable_Views
+
+class Custom_Reusable_ViewsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
